fe0f395ab2e320b4301acb0278e5cb6a599fd1a0
SadatArefin <sadat.arefin.rafat@gmail.com>
2023-06-03 11:23:41 +0600

    Model id instead of __id

--- app/models/product.model.js
+++ app/models/product.model.js
@@ -1,17 +1,21 @@
-module.exports = mongoose => {
-    const Product = mongoose.model(
-      "product",
-      mongoose.Schema(
-        {
-            name: String,
-            description: String,
-            price: Number,
-            quantity: Number,
-            status: Boolean      
-        },
-        { timestamps: true }
-      )
-    );
-  
-    return Product;
-  };
+module.exports = (mongoose) => {
+  var schema = mongoose.Schema(
+    {
+      name: String,
+      description: String,
+      price: Number,
+      quantity: Number,
+      status: Boolean,
+    },
+    { timestamps: true }
+  );
+
+  schema.method("toJSON", function () {
+    const { __v, _id, ...object } = this.toObject();
+    object.id = _id;
+    return object;
+  });
+
+  const Product = mongoose.model("product", schema);
+  return Product;
+};
